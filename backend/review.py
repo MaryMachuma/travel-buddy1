bd18f7dee0d27b234c1484a92c1a8d839b2ea09a
syowai638 <cmuthini77@gmail.com>
2025-04-09 22:46:02 +0300

    review.py

---
+++ backend/review.py
@@ -0,0 +1,19 @@
+from extensions import db
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    destination_id = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "destination_id": self.destination_id,
+            "rating": self.rating,
+            "comment": self.comment
+        }
