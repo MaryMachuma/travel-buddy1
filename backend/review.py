a1a97d88ed0bfdfbe3410c771659e8f1fa5fb051
MELODY SIMWA <simwamelody@gmail.com>
2025-04-10 10:34:24 +0300

    update code

---
+++ backend/review.py
@@ -0,0 +1,28 @@
+from extensions import db
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+    
+    # Define relationships
+    user = relationship("User", back_populates="reviews")
+    destination = relationship("Destination", back_populates="reviews")
+    
+    # Serialization rules
+    serialize_rules = ('-user.reviews', '-destination.reviews')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "destination_id": self.destination_id,
+            "rating": self.rating,
+            "comment": self.comment
+        }
---
+++ backend/trip.py
@@ -0,0 +1,17 @@
+from datetime import date
+from extensions import db
+
+
+class Trip(db.Model):
+    __tablename__ = 'trips'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))  
+    start_date = db.Column(db.Date)
+    budget = db.Column(db.Float)
+    note = db.Column(db.Text)
+    
+    # Define relationships
+    user = db.relationship("User", back_populates="trips")
+    destination = db.relationship("Destination", back_populates="trips")
