34ea06a74578d33a775c43f8b5cde65e8287d61a
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-04-14 11:23:51 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ app.config['JWT_SECRET_KEY'] = 'dev-jwt-secret-key'
 db.init_app(app)
 
 #CORS(app, resources={r"/*": {"origins": "https://travel-buddy-frontend-c7mp.onrender.com"}})
-CORS(app)  # while using Postman or local testing
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 migrate = Migrate(app, db)
 api = Api(app)
