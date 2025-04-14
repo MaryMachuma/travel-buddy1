fa0b4bd5386a61d65ca0380beac3255b871d8cf0
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-04-14 04:17:32 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,24 +3,20 @@ from flask_restful import Api, Resource
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate , upgrade
-from models import db, User, Destination, Trip 
+from models import User, Destination, Trip 
 from datetime import datetime
 from flask_cors import CORS  
 import os
 # App Configuration
 app = Flask(__name__)
 
-
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev-secret-key'
 app.config['JWT_SECRET_KEY'] = 'dev-jwt-secret-key'
 
-# Extensions Initialization
-db.init_app(app)
+db = SQLAlchemy(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 migrate = Migrate(app, db)
 api = Api(app)
 jwt = JWTManager(app)
