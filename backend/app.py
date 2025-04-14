7f82145f73535240d21c7938946a186a205f96fe
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-04-14 10:01:10 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -183,10 +183,81 @@ class TripResource(Resource):
                 "error": str(e)
             }, 500
 
+
+destinations_data = [
+    {
+        "id": 1,
+        "name": "Eiffel Tower",
+        "city": "Paris",
+        "country": "France",
+        "description": "The Eiffel Tower is the most iconic monument in Paris as it enchantingly adds elegance to the city skyline",
+        "image": "https://i.imgur.com/7E3dyCA.jpg",
+        "price": 400000,
+        "rating": 4.8
+    },
+    {
+        "id": 3,
+        "name": "The Great Wall of China",
+        "city": "Beijing",
+        "country": "China",
+        "description": "A captivating historical monument.It is a magical site and one of the 8 wonders of the world",
+        "image": "https://i.imgur.com/kb51UTd.jpg",
+        "price": 70000,
+        "rating": 4.5
+    },
+    {
+        "id": 4,
+        "name": "Watamu",
+        "city": "Mombasa",
+        "country": "Kenya",
+        "description": "Coastal village with white-sand beaches and vibrant marine life.",
+        "image": "https://i.imgur.com/zxiEde2.jpg",
+        "price": 65000,
+        "rating": 4.6
+    },
+    {
+        "id": 5,
+        "name": "Maldives Island",
+        "city": "Maldives",
+        "country": "Maldives",
+        "description": "Tropical paradise in the Indian Ocean renowned for its stunning coral atolls, crystal-clear lagoons, and diverse marine life.",
+        "image": "https://i.imgur.com/HLcA5TV.jpg",
+        "price": 400000,
+        "rating": 5.0
+    }
+]
+
+class SeedResource(Resource):
+    def post(self):
+        try:
+            # Delete existing destination records
+            Destination.query.delete()
+
+            # Seed new data
+            for data in destinations_data:
+                destination = Destination(
+                    id=data["id"],
+                    name=data["name"],
+                    city=data["city"],
+                    country=data["country"],
+                    description=data["description"],
+                    image=data["image"],
+                    price=data["price"],
+                    rating=data["rating"]
+                )
+                db.session.add(destination)
+
+            db.session.commit()
+            return {"message": "Destinations table seeded successfully"}, 201
+
+        except Exception as e:
+            return {"error": str(e)}, 500
+
 # API Routes
 api.add_resource(UserResource, '/register')
 api.add_resource(LoginResource, '/login')
 api.add_resource(DestinationsResource, '/destinations')
 api.add_resource(TripResource, '/trips')
+api.add_resource(SeedResource, '/seed')
 
 
